602c8c0d63641615772cdbffc86990e4c0367cf7
MCarmen <pmpalacios@iesfranciscodelosrios.es>
2025-11-05 14:51:02 +0100

    eliminar carpata de prueba

--- untitled/src/Ejemplo.java
+++
@@ -1,8 +0,0 @@
-public class Ejemplo {
-    public static void saludar(String nombre) {
-        System.out.println("Hola " + nombre);
-    }
-    public static void main(String[] args) {
-        saludar("Lucas");
-    }
-}
