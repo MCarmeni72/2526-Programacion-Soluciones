21b1923c4f3874126f7e9c9972109bd3c37aeeaa
Alfonso Jiménez <pjimenez@iesfranciscodelosrios.es>
2025-11-06 11:20:12 +0100

    Finalizado ejercicio 2

--- UT1/Arrays/src/Ejercicio2.java
+++ UT1/Arrays/src/Ejercicio2.java
@@ -15,17 +15,76 @@ public class Ejercicio2 {
         //1. Pedir numero de alumnos y crear array para ese numero de alumnos
         int numAlumnos = pideEntero("Introduce número de alumnos","Error, debes introducir un numero entero");
         int[] notas = new int[numAlumnos];
-        pideArray(notas);
+        // 2
+        pideArrayEnteros(notas);
+        // 3
+        System.out.println("Notas introducidas");
+        imprimeArray(notas);
+        // 4
+        System.out.println("Nota media: " + calcularMedia(notas));
+        // 5
+        System.out.println("La nota más alta es " + getMaximo(notas));
+        // 5
+        System.out.println("La nota más baja es " + getMinimo(notas));
     }
 
-    public static void pideArray (int [] array){
+    /**
+     * Devuelve 0 si el array está vacío
+     */
+    public static int getMaximo (int[] array) {
+        int maximo = 0;
+        if (array.length > 0) {
+            maximo = array[0];
+            for (int i = 1; i < array.length; i++) {
+                if (maximo < array[i]) {
+                    maximo = array[i];
+                }
+            }
+        }
+        return maximo;
+    }
+
+    /**
+     * Devuelve 0 si el array está vacío
+     */
+    public static int getMinimo (int[] array) {
+        int minimo = 0;
+        try {
+            minimo = array[0];
+            if (array.length > 0) {
+                for (int i = 1; i < array.length; i++) {
+                    if (minimo > array[i]) {
+                        minimo = array[i];
+                    }
+                }
+            }
+        } catch (IndexOutOfBoundsException e) {
+            System.out.println("ERROR: Array vacío. El mínimo devuelto será 0");
+            minimo = 0;
+        }
+        return minimo;
+    }
+
+    public static double calcularMedia(int[] array) {
+        double aculumador = 0;
+        for (int i = 0; i < array.length; i++) {
+            aculumador += array[i];
+        }
+        return aculumador / array.length;
+    }
+
+
+    public static void imprimeArray(int[] array) {
+        for(int i=0;i<array.length;i++){
+            System.out.println(array[i]);
+        }
+    }
+
+    public static void pideArrayEnteros(int [] array){
       for(int i=0;i<array.length;i++){
           array[i]=pideEntero("Introduce elemento "+i+": ","Error,El elemento debe ser un numero entero");
       }
     }
-
-
-
 
     public static int pideEntero( String mensaje, String mensajeError){
         Scanner sc = new Scanner(System.in);
