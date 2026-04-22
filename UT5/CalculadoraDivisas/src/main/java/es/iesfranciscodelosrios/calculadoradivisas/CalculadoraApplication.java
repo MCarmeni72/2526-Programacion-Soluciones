781b5e50797698a88e1e48d3b1fd2032caa7b5e1
Alfonso Jiménez <yo@alfonsojimenez.net>
2026-04-22 12:34:47 +0200

    CalculadoraDivisas terminada

--- UT5/CalculadoraDivisas/src/main/java/es/iesfranciscodelosrios/calculadoradivisas/CalculadoraApplication.java
+++ UT5/CalculadoraDivisas/src/main/java/es/iesfranciscodelosrios/calculadoradivisas/CalculadoraApplication.java
@@ -12,7 +12,7 @@ public class CalculadoraApplication extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(CalculadoraApplication.class.getResource("principal-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        stage.setTitle("Calculadora de divisas");
         stage.setScene(scene);
         stage.show();
     }
